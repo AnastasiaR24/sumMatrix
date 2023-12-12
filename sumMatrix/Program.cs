14be3020dde472ec4f402882d0408bf4ca964603
AnastasiaR24 <152877043+AnastasiaR24@users.noreply.github.com>
2023-12-12 16:30:50 +0300

    Update Program.cs

--- sumMatrix/Program.cs
+++ sumMatrix/Program.cs
@@ -12,56 +12,57 @@ namespace sumMatrix
         static void Main(string[] args)
         {
             Console.WriteLine("Введите количество строк для матриц:");
-            int rows = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите количество столбцов для матриц:");
-            int cols = Convert.ToInt32(Console.ReadLine());
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов для матриц:");
+int cols = Convert.ToInt32(Console.ReadLine());
 
-            int[,] matrix = new int[rows, cols];
-            int[,] matrixC = new int[rows, cols];
+int[,] matrixA = new int[rows, cols];
+int[,] matrixB = new int[rows, cols];
+int[,] matrixC = new int[rows, cols];
 
 // Создаются матрицы и заполняются рандомными числами
-            Random random = new Random();
-            Console.WriteLine("Первая матрица создана!");
-            for (int i = 0; i < rows; i++)
-               {
-                  for (int j = 0; j < cols; j++)
-                     {
-                        matrix[i, j] = random.Next(1, 101);  // Случайные целые числа от 1 до 100
-                        Console.Write(matrix[i, j] + " ");
-                     }
-                  Console.WriteLine();
-               }
+Random random = new Random();
+Console.WriteLine("Первая матрица создана!");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        matrixA[i, j] = random.Next(1, 101);  // Случайные целые числа от 1 до 100
+        Console.Write(matrixA[i, j] + " ");
+    }
+    Console.WriteLine();
+}
 
-             Console.WriteLine("Вторая матрица создана!");
-             for (int i = 0; i < rows; i++)
-                {
-                    for (int j = 0; j < cols; j++)
-                       {
-                          matrix[i, j] = random.Next(1, 101);  // Случайные целые числа от 1 до 100
-                          Console.Write(matrix[i, j] + " ");
-                       }
-                    Console.WriteLine();
-                 }
+Console.WriteLine("Вторая матрица создана!");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        matrixB[i, j] = random.Next(1, 101);  // Случайные целые числа от 1 до 100
+        Console.Write(matrixB[i, j] + " ");
+    }
+    Console.WriteLine();
+}
 // Сложение матриц
-             for (int i = 0; i < rows; i++)
-                {
-                    for (int j = 0; j < cols; j++)
-                       {
-                          matrixC[i, j] = matrix[i, j] + matrix[i, j];
-                       }
-  
-                }
-             Console.WriteLine("Результат сложения матриц A и B: ");
-             for (int i = 0; i < rows; i++)
-                {
-                    for (int j = 0; j < cols; j++)
-                       {
-                          Console.Write(matrixC[i, j] + " ");
-                       }
-                   Console.WriteLine();
-                 }
-             Console.ReadLine();
-            
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        matrixC[i, j] = matrixA[i, j] + matrixB[i, j];
+    }
+
+}
+Console.WriteLine("Результат сложения матриц A и B: ");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write(matrixC[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.ReadLine();
+
         }
     }
 }
